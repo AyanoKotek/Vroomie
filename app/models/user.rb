4c31727e5db04852beb81b79b4bd539eea7e9de0
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-03-13 15:31:35 +0100

    validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, :last_name, :email, :password, presence: true
   has_many :cars, :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
