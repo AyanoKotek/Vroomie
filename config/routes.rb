363defdaa5b19904831ceeca8859ae941185d795
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-03-14 17:03:27 +0100

    fixed delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :cars, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:index, :destroy, :show, :edit, :update]
+  resources :bookings, only: [:destroy, :show, :edit, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
