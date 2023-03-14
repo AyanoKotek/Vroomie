61fe7b458a3084b0e8afbbf5067515c8c502bad7
Hakim Benkacem <hbenkacem@yahoo.co.uk>
2023-03-14 18:05:43 +0400

    form to create a new instance of a car has been made

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,4 +7,20 @@ class CarsController < ApplicationController
   def new
     @car = Car.new
   end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to cars_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :description, :year, :price)
+  end
 end
