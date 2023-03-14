f39156b6f66e8ff88dfda40ef47a649c913ea515
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-03-14 11:55:34 +0100

    delete path in routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookings, only: :update
 end
