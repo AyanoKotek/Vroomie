a36d4d9ee1c8c6d56ac16aa1800a14a3c66cf798
francois vallat <vallatfrancois@yahoo.fr>
2023-03-14 15:37:40 +0100

    delete feature

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :set_car, only: [:new, :create]
+  before_action :set_car, only: [:new, :create, :show, :destroy]
 
   def index
     @bookings = Booking.all
@@ -24,7 +24,8 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to bookings_path, notice: "Booking was successfully deleted."
+
+    redirect_to root_path, status: :see_other, notice: "Booking was successfully deleted."
   end
 end
 
