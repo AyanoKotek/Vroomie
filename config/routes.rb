cf1338c21ced9042dd8c7b799c94b028dbe3588b
Hakim Benkacem <hbenkacem@yahoo.co.uk>
2023-03-14 13:40:31 +0400

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :cars, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create]
-  end
 end
