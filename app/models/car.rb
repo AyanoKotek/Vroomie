0c79543fa2040ebe61ec234b39c63712ada7e076
Hakim Benkacem <hbenkacem@yahoo.co.uk>
2023-03-16 15:29:50 +0400

    addded validation and custom message on error

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+  validates :photos, :length => { :minimum => 3, :maximum => 3,
+  too_short: ": not enough images must upload 3.",
+  too_long: ": too many images must upload 3."
+ }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
